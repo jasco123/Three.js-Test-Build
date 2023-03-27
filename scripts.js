49db7096da25b4daaa0ad7214ab6b1e54460c46b
Jesus Medina Obledo <86178388+jasco123@users.noreply.github.com>
2023-03-27 00:48:47 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-
-
-import * as THREE from 'three';
+import * as THREE from 'https://threejs.org/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
